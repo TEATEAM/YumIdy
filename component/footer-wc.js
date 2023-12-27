11346be91db58db17bd8f5a8c24144727a605ecf
Turuux <121284433+Turuux@users.noreply.github.com>
2023-12-27 15:54:25 +0800

    header, footer components

--- component/footer-wc.js
+++ component/footer-wc.js
@@ -1,25 +1,40 @@
-class FooterWc extends HTMLElement {
+class Footercomponent extends HTMLElement {
     constructor() {
         super();
-        //implementation
+        this.#render();
     }
 
     connectedCallback() {
-        //implementation
+
     }
 
-    disconnectedCallback() {
-        //implementation
+    #render() {
+        this.innerHTML =`
+        <footer>
+       
+      <nav class="footer"  >
+         
+          <a href="https://www.facebook.com" class="fb">
+              <i class="fa-brands fa-facebook-f"></i> </a>
+  
+  
+          <a href="https://www.twitter.com" class="twitter" >
+              <i class="fa-brands fa-twitter"></i> </a>
+  
+  
+          <a href="https://www.instagram.com" class="insta">
+              <i class="fa-brands fa-instagram"></i> </a>
+          
+      </nav>
+      
+      <article class="end">
+         
+          &copy; 2023 <span style="color:#EE5151;">Yum</span>idy. All Rights Reserved.
+      </article>
+  
+  </footer>
+        `
     }
-
-    attributeChangedCallback(name, oldVal, newVal) {
-        //implementation
-    }
-
-    adoptedCallback() {
-        //implementation
-    }
-
 }
 
-window.customElements.define('footer-wc', FooterWc);
+window.customElements.define('footer-component', Footercomponent);
