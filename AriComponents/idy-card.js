4851329d7a88b7ef363f6bca6dfd873602f1d164
erdnzl <erdnzlgb@gmail.com>
2024-02-02 17:34:40 +0800

    test

--- AriComponents/idy-card.js
+++ AriComponents/idy-card.js
@@ -1,84 +1,108 @@
+const template = document.createElement("template");
+
+template.innerHTML = `
+    <article class="idy-card">
+        <img class="foodImg">
+        <div class="cardInfo">
+            <h4 class="resName"></h4>
+            <idy-like-btn></idy-like-btn>
+        </div>
+        <div class="cardInfo1">
+            <i class="fa-regular fa-clock" id="tsagLogo"></i>
+            <p class="time"></p>
+            <p class="price"></p>
+        </div>
+        <p class="foodType"></p>
+        <slot name="remove"></slot>
+    </article>
+`
 class IdyCard extends HTMLElement {
     constructor() {
         this.attachShadow({mode:'open'});
         //templete-iin talaar
-        
+        this.shadowRoot.appendChild(template.content.cloneNode(true));
         // club-like-btn component-оос шидсэн эвентийг барьж авах хэсэг
-        this.addEventListener("heart-btn-liked", ()=>this.heartBtnClicked(true));
-        this.addEventListener("heart-btn-disliked", ()=>this.heartBtnClicked(false));
-        this.addEventListener("click", (e)=>this.clicked(e));
+        // this.addEventListener("heart-btn-liked", ()=>this.heartBtnClicked(true));
+        // this.addEventListener("heart-btn-disliked", ()=>this.heartBtnClicked(false));
+        // this.addEventListener("click", (e)=>this.clicked(e));
     }
 
-    heartBtnClicked(val){
-        console.log("received");
+    // heartBtnClicked(val){
+    //     console.log("received");
         
-        let Resdatas = {
-            id: thi.getAttribute("id"),
-            name: this.getAttribute("name"),
-            // stars: this.getAttribute("stars"),
-            coverImg: this.getAttribute("coverImg"),
-            type: this.getAttribute("type"),
-            timeIcon: this.getAttribute("timeIcon"),
-            openOrNO: this.getAttribute("openOrNO"),
-            price: this.getAttribute("price")
-        };
+    //     let Resdatas = {
+    //         id: thi.getAttribute("id"),
+    //         name: this.getAttribute("name"),
+    //         // stars: this.getAttribute("stars"),
+    //         coverImg: this.getAttribute("coverImg"),
+    //         type: this.getAttribute("type"),
+    //         timeIcon: this.getAttribute("timeIcon"),
+    //         openOrNO: this.getAttribute("openOrNO"),
+    //         price: this.getAttribute("price")
+    //     };
 
-        localStorage.setItem(this.getAttribute("name"), val);
+    //     localStorage.setItem(this.getAttribute("name"), val);
 
-        //club-н картууд зүрхлэгдсэн бол түүнийг мэдээллэх зорилгоор CustomEvent бэлдэж байна.
+    //     //club-н картууд зүрхлэгдсэн бол түүнийг мэдээллэх зорилгоор CustomEvent бэлдэж байна.
 
-        const event = new CustomEvent(
-            'card-heart-clicked',
-            {
-                composed: true,
-                detail: {
-                    name: this.name,
-                    isLiked: val,
-                    theRes: Resdatas
-                }
-            }
-        );
+    //     const event = new CustomEvent(
+    //         'card-heart-clicked',
+    //         {
+    //             composed: true,
+    //             detail: {
+    //                 name: this.name,
+    //                 isLiked: val,
+    //                 theRes: Resdatas
+    //             }
+    //         }
+    //     );
 
-        window.dispatchEvent(event);
+    //     window.dispatchEvent(event);
 
-        if(!val){
-            this.shadowRoot.querySelector("idy-like-btn").removeAttribute("checked");
-        }
-    }
+    //     if(!val){
+    //         this.shadowRoot.querySelector("idy-like-btn").removeAttribute("checked");
+    //     }
+    // }
 
-    clicked(e){
-        const isIdyHeartBtn = e.composedPath().includes(this.shadowRoot.querySelector("idy-like-btn"));
+    // clicked(e){
+    //     const isIdyHeartBtn = e.composedPath().includes(this.shadowRoot.querySelector("idy-like-btn"));
 
-        if(!isIdyHeartBtn) {
-            const resId = this.getAttribute("id");
-            window.location.href = `./aboutRes?id=${resId}`;
-        }
-    }
-    connectedCallback() {
-        let likedState = localStorage.getItem(this.getAttribute("name"));
-        if(likedState === "true"){
-            this.shadowRoot.querySelector("idy-like-btn").setAttribute("checked",true);
-        }
-    }
+    //     if(!isIdyHeartBtn) {
+    //         const resId = this.getAttribute("id");
+    //         window.location.href = `./aboutRes?id=${resId}`;
+    //     }
+    // }
+    // connectedCallback() {
+    //     let likedState = localStorage.getItem(this.getAttribute("name"));
+    //     if(likedState === "true"){
+    //         this.shadowRoot.querySelector("idy-like-btn").setAttribute("checked",true);
+    //     }
+    // }
 
     static get observedAttributes() {
-        return["id", "name", "coverImg", "type", "timeIcon", "openOrNO", "price"];
+        return["id", "name", "img", "type", "time", "price"];
     }
 
     attributeChangedCallback(name, oldVal, newVal) {
         switch(name){
             case "id":
                 this.shadowRoot.id = newVal;
-            case "name":
-                this.shadowRoot.querySelector(".cardInfo .resName").innerHTML = newVal;
                 break;
-            case "coverImg":
-                this.shadowRoot.querySelector("img").src = newVal;
-            case "type": 
-                this.shadowRoot.querySelector(".foodType").innerHTML = newVal;
-            case "timeIcon":
-                this.shadowRoot.querySelector(".cardInfo1 #tsagLogo").src = newVal;
-                
+            case "name":
+                this.shadowRoot.querySelector(".resName h4").innerHTML = newVal;
+                break;
+            case "img":
+                this.shadowRoot.querySelector(".foodImg").src = newVal;
+                break;
+            case "type":
+                this.shadowRoot.querySelector(".foodType p").innerHTML = newVal;
+                break;
+            case "time":
+                this.shadowRoot.querySelector(".time p").innerHTML = newVal;
+                break;
+            case "price":
+                this.shadowRoot.querySelector(".price p").innerHTML = newVal;
+                break;
         }
     }
 
