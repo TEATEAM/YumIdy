27e93d7ea493142cb71e272e3f6db25bec4f0ac3
erdnzl <erdnzlgb@gmail.com>
2024-02-07 13:18:06 +0800

    node

--- public/api/db/db.mjs
+++ public/api/db/db.mjs
@@ -1,5 +1,5 @@
 import postgres from 'postgres'
 
 const sql = postgres('postgres://postgres:1719@localhost:5432/postgres');
-
+                      //server//user: password:port:db name
 export default sql;
